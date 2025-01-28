fe4dcbe810de653996533c0112d38492a1d6a639
Sarthak Bhatnagar <sarthakbhatnagar103@gmail.com>
2025-01-29 03:33:40 +0530

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,44 @@
+import video1 from "../assets/video1.gif";
+import video2 from "../assets/video2.gif";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualHq  
+        <h2 className="bg-gradient-to-r from-green-300 to-green-800 text-transparent bg-clip-text">
+          {" "}
+          Your Interactive Virtual Workspace
+        </h2>
+      </h1>
+      <p className="mt-10 text-lg text-center text-white-500 max-w-4xl">
+      Transform the Way You Connect: Build and Explore Virtual Spaces for Work, Play, and Collaboration with VirtualHQ! 
+      </p>
+      <div className="flex justify-center my-10">
+        <a
+          href="#"
+          className="bg-gradient-to-r from-teal-500  to-teal-800 py-3 px-4 mx-3 rounded-md"
+        >
+          Start your Free Trial
+        </a>
+        <a href="#" className="bg-gradient-to-r from-teal-500  to-teal-800 py-3 px-4 mx-3 rounded-md ">
+          Have a Tour
+        </a>
+      </div>
+      <div className="flex mt-10 justify-center">
+          <img
+            src={video1}
+            alt="Animated GIF"
+            className="rounded-lg"
+          />
+          <img
+            src={video2}
+            alt="Animated GIF"
+            className="rounded-lg"
+          />
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
